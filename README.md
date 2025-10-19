cc916f5f2beca53465c73db4cba7401e727b9fa9
Shrawankumar <sunnybtech4@gmail.com>
2025-10-19 14:21:10 +0530

    config server

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # config-server
+
+# to test the config value present in centralconfig
+http://localhost:8888/global/dev/main
+http://localhost:8888/order-service/dev/main
+
+- localhost:8888/<application>/<profile><branch>
---
+++ src/test/java/com/codeing/practice/configs_server/ConfigsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeing.practice.configs_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
